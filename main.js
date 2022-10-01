d0884a82cbe65ec06e78987666f7575e159e7d68
wowblast <jfrt142012@hotmail.com>
2022-10-01 11:59:31 -0400

    Add background to the scene

--- main.js
+++ main.js
@@ -3,24 +3,68 @@ import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 import * as THREE from 'three';
 
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+import starsTexture from './img/stars.jpg';
+import sunTexture from './img/sun.jpg';
+import mercuryTexture from './img/mercury.jpg';
+import venusTexture from './/img/venus.jpg';
+import earthTexture from './img/earth.jpg';
+import marsTexture from './img/mars.jpg';
+import jupiterTexture from './img/jupiter.jpg';
+import saturnTexture from './img/saturn.jpg';
+import saturnRingTexture from './img/saturn ring.png';
+import uranusTexture from './img/uranus.jpg';
+import uranusRingTexture from './img/uranus ring.png';
+import neptuneTexture from './img/neptune.jpg';
+import plutoTexture from './img/pluto.jpg';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
 const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
-setupCounter(document.querySelector('#counter'))
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(-90, 140, 140);
+orbit.update();
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture
+]);
+
+const textureLoader = new THREE.TextureLoader();
+
+function animate() {
+  //Self-rotation
+ 
+
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
