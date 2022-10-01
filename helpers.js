567ecbe1c99becd8612094a6a1249f1424ddb635
Adriana Orellana <adrianaorellana34@gmail.com>
2022-10-01 15:14:27 -0400

    Update searcher container styles and clean code

--- helpers.js
+++ helpers.js
@@ -1,37 +1,23 @@
 import * as THREE from "three";
 
-export function createPlanet(texture, textureRing,textureLoader) {
-    const size = getRandom(5,30);
-    
-    const geo = new THREE.SphereGeometry(size, 30, 30);
-    const mat = new THREE.MeshStandardMaterial({
-        map: textureLoader.load(texture[getRandom(0,texture.length-1)])
-    });
-    const mesh = new THREE.Mesh(geo, mat);
-    const obj = new THREE.Object3D();
-    obj.add(mesh);
-    if(getRandom(1,10) > 8) {
-        const ringGeo = new THREE.RingGeometry(
-            size,
-            size + getRandom(5,20),
-            32);
-        const ringMat = new THREE.MeshBasicMaterial({
-            map: textureLoader.load(textureRing[getRandom(0,textureRing.length-1)]),
-            side: THREE.DoubleSide
-        });
-        const ringMesh = new THREE.Mesh(ringGeo, ringMat);
-        obj.add(ringMesh);
-        ringMesh.position.x = innerWidth-1;
-        ringMesh.rotation.x = -0.5 * Math.PI;
-    }
-    mesh.position.x = getRandom(-500,500)
-    mesh.position.y = getRandom(-500,500)
+export function createPlanet(texture, textureLoader) {
+  const size = getRandom(5, 30);
 
-    mesh.position.z =  getRandom(-500,500)
+  const geo = new THREE.SphereGeometry(size, 30, 30);
+  const mat = new THREE.MeshStandardMaterial({
+    map: textureLoader.load(texture[getRandom(0, texture.length - 1)]),
+  });
+  const mesh = new THREE.Mesh(geo, mat);
+  const obj = new THREE.Object3D();
+  obj.add(mesh);
 
-    return {mesh, obj}
+  mesh.position.x = getRandom(-500, 500);
+  mesh.position.y = getRandom(-500, 500);
+  mesh.position.z = getRandom(-500, 500);
+
+  return { mesh, obj };
 }
 
 export function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
--- main.js
+++ main.js
@@ -1,40 +1,46 @@
 import "./style.css";
 import * as THREE from "three";
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { createPlanet, getRandom } from './helpers'
-import starsTexture from './img/stars.jpg';
-import sunTexture from './img/sun.jpg';
-import mercuryTexture from './img/mercury.jpg';
-import venusTexture from './/img/venus.jpg';
-import earthTexture from './img/earth.jpg';
-import marsTexture from './img/mars.jpg';
-import jupiterTexture from './img/jupiter.jpg';
-import saturnTexture from './img/saturn.jpg';
-import saturnRingTexture from './img/saturn ring.png';
-import uranusTexture from './img/uranus.jpg';
-import uranusRingTexture from './img/uranus ring.png';
-import neptuneTexture from './img/neptune.jpg';
-import plutoTexture from './img/pluto.jpg';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { createPlanet, getRandom } from "./helpers";
+import starsTexture from "./img/stars.jpg";
+import sunTexture from "./img/sun.jpg";
+import mercuryTexture from "./img/mercury.jpg";
+import venusTexture from ".//img/venus.jpg";
+import marsTexture from "./img/mars.jpg";
+import jupiterTexture from "./img/jupiter.jpg";
+import saturnTexture from "./img/saturn.jpg";
+import uranusTexture from "./img/uranus.jpg";
+import neptuneTexture from "./img/neptune.jpg";
+import plutoTexture from "./img/pluto.jpg";
 
-const textures = [mercuryTexture, venusTexture, marsTexture, jupiterTexture,
-  saturnTexture, uranusTexture, neptuneTexture, plutoTexture];
-const ringTextures = [saturnRingTexture, uranusRingTexture];
+const textures = [
+  mercuryTexture,
+  venusTexture,
+  marsTexture,
+  jupiterTexture,
+  saturnTexture,
+  uranusTexture,
+  neptuneTexture,
+  plutoTexture,
+];
 const planets = [];
-
 
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
+renderer.setSize(sizes.width, sizes.height);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-
 const camera = new THREE.PerspectiveCamera(
   45,
-  window.innerWidth / window.innerHeight,
+  sizes.width / sizes.height,
   0.1,
   1000
 );
@@ -54,58 +60,46 @@ scene.background = cubeTextureLoader.load([
   starsTexture,
   starsTexture,
   starsTexture,
-  starsTexture
+  starsTexture,
 ]);
 
 const textureLoader = new THREE.TextureLoader();
 
-
 const sunGeo = new THREE.SphereGeometry(16, 30, 30);
 const sunMat = new THREE.MeshBasicMaterial({
-  map: textureLoader.load(sunTexture)
+  map: textureLoader.load(sunTexture),
 });
 const sun = new THREE.Mesh(sunGeo, sunMat);
-console.log(innerWidth)
-//sun.position = 50
 scene.add(sun);
+
 for (let index = 0; index < 50; index++) {
-  const planet = createPlanet(textures, ringTextures, textureLoader)
+  const planet = createPlanet(textures, textureLoader);
   planets.push(planet);
-  scene.add(planet.obj)
-
+  scene.add(planet.obj);
 }
-const mars = createPlanet(textures, ringTextures, textureLoader);
-scene.add(mars.obj)
-// const planet = createPlanet(textures,ringTextures)
-//console.log(planet)
-//scene.add(planet.obj)
-// scene.add(planet.mesh)
 
-const pointLight = new THREE.PointLight(0xFFFFFF, 2, 500);
+const mars = createPlanet(textures, textureLoader);
+scene.add(mars.obj);
+
+const pointLight = new THREE.PointLight(0xffffff, 2, 500);
 scene.add(pointLight);
-function animate() {
-  //Self-rotation
-planets.forEach(planet => {
-  //console.log(planet)
-  planet.mesh.rotateY(getRandom(1,10)/1000);
-  //planet.obj.rotateY(getRandom(1,10)/10000);
 
-});
- 
+function animate() {
+  planets.forEach((planet) => {
+    planet.mesh.rotateY(getRandom(1, 10) / 1000);
+  });
+
   renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
+window.addEventListener("resize", function () {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 
-  //renderer.render(scene, camera);
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(sizes.width, sizes.height);
 });
-
-
-
-
